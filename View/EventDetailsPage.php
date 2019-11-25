6d84cb007b39a2e703aee3c5b580809349dc9aa2
Darboux12 <dariuszbula98@gmail.com>
2019-11-25 15:01:00 +0100

    Adding Event Details Page Commit

---
+++ View/EventDetailsPage.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="Stylesheet" type="text/css" href="../Public/CSS/PagesStyle/GeneralStyle.css">
+    <link rel="Stylesheet" type="text/css" href="../Public/CSS/PagesStyle/EventDetailsPageStyle.css">
+    <link rel="Stylesheet" type="text/css" href="../Public/CSS/ElementsStyle/TopBarStyle.css">
+    <link rel="Stylesheet" type="text/css" href="../Public/CSS/ElementsStyle/ChatBoxStyle.css">
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <title>Event Details</title>
+
+</head>
+
+
+<body>
+
+<button class="chatbox-open-button" onclick="openChat()">Chat</button>
+
+<div id="chat" class="chat-container">
+
+    <div class="search-container">
+        <input type="text" placeholder="Szukaj" onfocus="this.placeholder = ''">
+    </div>
+
+    <div class="coversation-list"></div>
+
+    <div class="new-message-container">
+        <a href="#add"><i class="fas fa-plus-circle"></i></a>
+    </div>
+
+    <div class="chat-title">
+        <span>John Doe</span>
+        <button class="close-chat-button" onclick="closeChat()">X</button>
+    </div>
+
+    <div class="chat-message-list">chat-message-list</div>
+
+    <div class="chat-form">chat-form</div>
+
+</div>
+
+<div class="Wrapper">
+
+    <div class="TopBar">
+
+        <div id="logo">
+            <a  href="#Page"><img src="../Public/Images/logo-no-writing.png"></a>
+        </div>
+
+        <div class="Options">
+
+            <a href="#page"><i class="fas fa-globe"></i>Aktualności</a>
+            <a href="#page"><i class="fas fa-map-marked-alt"></i>Oznacz miejsce</a>
+            <a href="CreateEventPage.php"><i class="far fa-calendar-plus"></i>Utwórz wydarzenie</a>
+
+        </div>
+
+        <div id="Notification">
+            <a href="#page"><i class="fas fa-user"></i></a>
+            <a href="#page"><i class="fas fa-heart"></i></a>
+            <a href="#page"><i class="fas fa-bell"></i></a>
+        </div>
+
+    </div>
+
+    <div class="EventInformation">
+
+        <div class="EventInformationFieldContainer">
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+            <div class="InformationField">
+
+                <h id="FieldName">Nazwa wydarzenia</h>
+                <h id="FieldInfo">Siatkówka dla pań</h>
+
+            </div>
+
+        </div>
+
+        <img src="../Public/Images/Kamienna.png">
+
+        <div class="EventDescription">
+            <h>O wydarzeniu</h>
+            <textarea rows="20" readonly>To jest pole tekstowe</textarea>
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+    <div class="RightNav">RightNav</div>
+
+</div>
+
+
+
+
+
+
+
+<script>
+    function openChat() {document.getElementById("chat").style.display = "grid";}
+    function closeChat() {document.getElementById("chat").style.display = "none";}
+</script>
+
+
+</body>
+</html>
