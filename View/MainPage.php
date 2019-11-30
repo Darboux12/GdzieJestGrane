be099ac593c0946ed161d72b422692d47fd88ac3
Darboux12 <dariuszbula98@gmail.com>
2019-11-30 20:34:55 +0100

    Mobile Top Bar First Step Commit

--- View/MainPage.php
+++ View/MainPage.php
@@ -5,16 +5,13 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="Stylesheet" type="text/css" href="../Public/CSS/PagesStyle/GeneralStyle.css">
     <link rel="Stylesheet" type="text/css" href="../Public/CSS/PagesStyle/MainPageStyle.css">
     <link rel="Stylesheet" type="text/css" href="../Public/CSS/ElementsStyle/TopBarStyle.css">
     <link rel="Stylesheet" type="text/css" href="../Public/CSS/ElementsStyle/ChatBoxStyle.css">
     <link rel="Stylesheet" type="text/css" href="../Public/CSS/ElementsStyle/CheckBoxStyle.css">
     <link rel="Stylesheet" type="text/css" href="../Public/CSS/ElementsStyle/PopularEventsNav.css">
-
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-
     <title>Main Page</title>
 
 </head>
@@ -53,72 +50,197 @@
 
     <div class="TopBar">
 
-        <a href="#Page"><img id="Ball-Image" src="../Public/Images/logo-no-writing.png"></a>
+        <i id="Bar" class="fas fa-bars" onclick="ToggleMobileMenuBar()"></i>
 
-        <i class="fas fa-bars"></i>
+        <i class="fas fa-heart"></i>
 
-        <a id="News-Link" href="#page"><i class="fas fa-globe"></i>Aktualności</a>
+        <a class="Ball-link" href="#Page"><img id="Ball-Image" src="../Public/Images/logo-no-writing.png"><h>GdzieJestGrane</h></a>
 
-        <a href="#page"><i class="fas fa-map-marked-alt"></i>Oznacz miejsce</a>
+        <div id="Mobile-Drop-Menu-Bar" class="Mobile-Drop-Menu-Bar">
 
-        <a id="MakeEvent-Link" href="CreateEventPage.php"><i class="far fa-calendar-plus"></i>Utwórz wydarzenie</a>
+            <div class="ProfileLinks">
 
-        <div class="UserMenu">
+                <a href="#page"><i class="fas fa-calendar-alt"></i><h>Moje wydarzenia</h></a>
 
-            <i  id="usericon" class="fas fa-user" onclick="ToggleUserMenu()"></i>
+                <a href="#page"><i class="fas fa-map"></i><h>Oznaczone miejsca</h></a>
 
-            <div id="myBubble" class="bubble">
+                <a id="CreateEvent" href="CreateEventPage.php"><i class="far fa-calendar-plus"></i><h>Utwórz wydarzenie</h></a>
 
-                <form>
-                    <div class="UserProfile">
-                        <img src="../Public/Images/Person1.jpg">
+                <a href="#page"><i class="fas fa-map-marked-alt"></i><h>Oznacz miejsce</h></a>
 
-                        <div class="NameSeeProfile">
-                            <h id="Name">Janek126p</h>
-                            <h id="SeeProfile">Zobacz profil</h>
-                        </div>
+                <a href="#page"><i class="fas fa-bell"></i><h>Powiadomienia</h></a>
 
-                        <i class="fas fa-chevron-circle-right"></i>
-
-                    </div>
-
-                    <div class="ProfileLinks">
-
-                        <div class="ProfileLinks-link">
-                            <a href="#page">Moje wydarzenia</a>
-                            <i class="fas fa-chevron-right"></i>
-                        </div>
-
-                        <div class="ProfileLinks-link">
-                            <a href="#page">Oznaczone miejsca</a>
-                            <i class="fas fa-chevron-right"></i>
-                        </div>
-
-                        <div class="ProfileLinks-link">
-                            <a href="#page">Znajomi</a>
-                            <i class="fas fa-chevron-right"></i>
-                        </div>
-
-                        <div class="ProfileLinks-link">
-                            <a href="#page">Ustawienia</a>
-                            <i class="fas fa-chevron-right"></i>
-                        </div>
-
-                        <div class="ProfileLinks-link">
-                            <a href="#page">Wyloguj</a>
-                            <i class="fas fa-chevron-right"></i>
-                        </div>
-
-                    </div>
-                </form>
+                <a href="#page"><i class="fas fa-globe"></i><h>Aktualności</h></a>
 
             </div>
 
         </div>
 
-        <i class="fas fa-heart"></i>
+        <i id="MobileSearch" onclick="ToggleMobileMenuSearch()" class="fas fa-search"></i>
 
-        <i class="fas fa-bell"></i>
+        <i  id="usericon" class="fas fa-user" onclick="ToggleMobileMenuUser()"></i>
+
+        <div id="Mobile-Drop-Menu-User" class="Mobile-Drop-Menu-User">
+
+            <div class="UserProfile">
+
+                <img src="../Public/Images/Person1.jpg">
+
+                <div class="NameSeeProfile">
+                    <h id="Name">Janek126p</h>
+                </div>
+
+                <a href="#page"><h>Zobacz profil</h><i class="fas fa-chevron-circle-right"></i></a>
+
+            </div>
+
+            <div class="ProfileLinks">
+
+                <a href="#page"><i class="fas fa-user-friends"></i><h>Znajomi</h></a>
+
+                <a href="#page"><i class="fas fa-cog"></i><h>Ustawienia</h></a>
+
+                <a href="#page"><i class="fas fa-sign-out-alt"></i><h>Wyloguj</h></a>
+
+
+
+            </div>
+
+
+
+        </div>
+
+        <div id="Mobile-Drop-Menu-Search" class="Mobile-Drop-Menu-Search">
+
+            <div id="SelectSport" class="SearchNavSelect">
+                <select>
+                    <option value="0">Wybierz dyscyplinę</option>
+                    <option value="1">Piłka Nożna</option>
+                    <option value="2">Siatkówka</option>
+                    <option value="3">Koszykówka</option>
+                    <option value="4">Pływanie</option>
+                    <option value="5">Kajakarstwo</option>
+                    <option value="6">Golf</option>
+                    <option value="7">Bieganie</option>
+                    <option value="8">Inne</option>
+                </select>
+                <i class="fas fa-caret-down"></i>
+            </div>
+
+            <div id="SelectState" class=SearchNavSelect>
+                <select>
+                    <option value="0">Wybierz województwo</option>
+                    <option value="1">Dolnosląskie</option>
+                    <option value="2">Kujawsko-pomorskie</option>
+                    <option value="3">Lubelskie</option>
+                    <option value="4">Lubuskie</option>
+                    <option value="5">Łódzkie</option>
+                    <option value="6">Małopolskie</option>
+                    <option value="7">Mazowieckie</option>
+                    <option value="8">Opolskie</option>
+                    <option value="9">Podkarpackie</option>
+                    <option value="10">Podlaskie</option>
+                    <option value="11">Warmińsko-mazurskie</option>
+                    <option value="12">Wielkopolskie</option>
+                    <option value="13">Zachodnio-pomorskie</option>
+                </select>
+                <i class="fas fa-caret-down"></i>
+            </div>
+
+            <div id="SelectCity" class=SearchNavSelect>
+                <select>
+                    <option value="0">Wybierz miasto</option>
+                    <option value="1">Kraków</option>
+                    <option value="2">Częstochowa</option>
+                    <option value="3">Tarnów</option>
+                    <option value="4">Oświęcim</option>
+                    <option value="5">Zakopane</option>
+                    <option value="6">Chrzanów</option>
+                    <option value="7">Wieliczka</option>
+                </select>
+                <i class="fas fa-caret-down"></i>
+            </div>
+
+            <div class="SearchBar-SelectNumber">
+
+                <h>Podaj cenę:</h>
+
+                <div class="SelectInputs">
+                    <input id="from" type="number" placeholder="od..." onfocus="this.placeholder=''" onblur="this.placeholder='od...'" min="0" step="any" />
+                    <input id="to"  type="number" placeholder="do..." onfocus="this.placeholder=''" onblur="this.placeholder='do...'" min="0" step="any" />
+                </div>
+
+            </div>
+
+            <div class="SearchBar-SelectNumber">
+
+                <h>Podaj liczbę osób:</h>
+
+                <div class="SelectInputs">
+                    <input id="NumebrOfPeopleInput" type="number" placeholder="Liczba osób..." onfocus="this.placeholder=''" onblur="this.placeholder='Liczba osób...'" min="0" step="any" />
+                </div>
+
+            </div>
+
+            <div class="SearchBar-SelectNumber">
+
+                <h>Podaj datę wydarzenia:</h>
+
+                <div class="SelectInputs">
+                    <input id="NumebrOfPeopleInput" type="date"/>
+                </div>
+
+            </div>
+
+            <div class="CheckBox">
+
+                <h>Szukam:</h>
+
+                <div class="Checkbox-options">
+
+                    <div class="Checkbox-option-container">
+
+                        <label class="Checkbox-option">
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                            <h>Boiska</h>
+                        </label>
+
+                    </div>
+
+                    <div class="Checkbox-option-container">
+
+                        <label class="Checkbox-option">
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                            <h>Hali</h>
+                        </label>
+
+                    </div>
+
+
+                    <div class="Checkbox-option-container">
+
+                        <label id="last-box" class="Checkbox-option">
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                            <h>Terenu</h>
+                        </label>
+
+                    </div>
+
+
+                </div>
+
+
+
+
+            </div>
+
+            <button id="SubmitButton" type="submit">Wyszukaj</button>
+
+
+        </div>
 
     </div>
 
@@ -665,6 +787,8 @@
 
 
 
+
+
 <script>
 
     function openChat() {document.getElementById("chat").style.display = "grid";}
@@ -692,22 +816,37 @@
     })
 
 
+    function ToggleMobileMenuBar() {
 
+        var bubble = document.getElementById('Mobile-Drop-Menu-Bar');
 
+        bubble.classList.toggle("show");
+    }
 
+    function ToggleMobileMenuUser() {
 
+        var bubble = document.getElementById('Mobile-Drop-Menu-User');
 
+        bubble.classList.toggle("show");
+    }
 
+    function ToggleMobileMenuSearch() {
 
+        var bubble = document.getElementById('Mobile-Drop-Menu-Search');
 
-
-
+        bubble.classList.toggle("show");
+    }
 
 
 </script>
 
 
 
-</body>
+
+
+
+
+
+    </body>
 
 </html>
